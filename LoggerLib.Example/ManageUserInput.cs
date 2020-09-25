e91b466039f1536b7a351d466272fe7cf6bad86c
sayedmhussein <sayed.hussein@gmx.com>
2020-09-25 11:16:36 +0300

    Update ManageUserInput.cs

--- LoggerLib.Example/ManageUserInput.cs
+++ LoggerLib.Example/ManageUserInput.cs
@@ -5,13 +5,22 @@ namespace LoggerLib.Example
 {
     public class ManageUserInput
     {
+        private readonly string messageToDisplay;
+
         public ManageUserInput(string input)
         {
+            switch (input)
+            {
+                case "":
+                    break;
+                default:
+                    break;
+            }
         }
 
         public string GetMessageToDisplay()
         {
-            return "Hello World";
+            return messageToDisplay;
         }
     }
 }
